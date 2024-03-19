8ba23f1716fd6e610db2a19ce3857cf46adcad9a
Conn <cobreath@tcd.ie>
2024-03-19 18:15:30 +0000

    Initial commit

---
+++ minif2f_import.lean
@@ -0,0 +1,11 @@
+import Mathlib.Algebra.BigOperators.Basic
+import Mathlib.Data.Complex.Basic
+import Mathlib.Data.Complex.Exponential
+import Mathlib.Data.Nat.Digits
+import Mathlib.Data.Nat.Log
+import Mathlib.Data.Real.Basic
+import Mathlib.Data.ZMod.Basic
+import Mathlib.Data.ZMod.Defs
+import Mathlib.NumberTheory.Divisors
+import Mathlib.Topology.Basic
+
